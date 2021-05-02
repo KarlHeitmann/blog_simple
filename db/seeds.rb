9777a77c70a7540cb2eeab2e26ab916978c0effa
Karl Heitmann <khf1988@gmail.com>
2021-05-01 21:17:45 -0400

    agregado semillas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'user1@example.com', password: 'asdasd', name: 'User 1', last_name: 'Last name user 1')
