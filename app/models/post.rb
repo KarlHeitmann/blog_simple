045bd0cfc7000538322b8bb4d520da30b471f95f
Karl Heitmann <khf1988@gmail.com>
2021-05-04 20:46:33 -0400

    implementado infinite scroll

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def next
+    self.class.where("id > ?", self.id).order(id: :asc).first
+  end
+
+  def previous
+    self.class.where("id < ?", self.id).order(id: :desc).first
+  end
+
 end
