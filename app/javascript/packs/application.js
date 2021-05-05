cfd6e92a6a6c263e936f3ad1c84e3b4e930969c7
Karl Heitmann <khf1988@gmail.com>
2021-05-04 20:41:50 -0400

    instalado turbo-rails, e instalado turbo con 'rails turbo:install'

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
@@ -12,7 +12,6 @@ import "bootstrap"
 import "./main.scss"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 async function getBitcoinData() {
